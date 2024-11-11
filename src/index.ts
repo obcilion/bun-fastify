0f0784236c0d8ae6429fe161ea8cc51bef090024
Raymond Gulbrandsen <gulbrandsen@fastmail.com>
2024-11-11 11:50:41 +0100

    🔧 Read port from $PORT, or default to 3000

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,10 @@ fastify.register(routes);
  * Run the server!
  */
 const start = async () => {
+  const parsedPort = parseInt(process.env["PORT"] ?? "");
+  const port = Number.isNaN(parsedPort) ? 3000 : parsedPort;
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: port });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
